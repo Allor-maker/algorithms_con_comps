57274b1d0abd50f55394d149f17c38a10cdce2c4
P_Alex_Tr <aleksandr.smyshlaev@yandex.ru>
2025-10-19 19:33:46 +0300

    zero-commit

---
+++ labs/lab1/include/calc/calc.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+
+class Graph
+{
+private:
+
+	std::vector<std::pair<int, int>> edges;
+public:
+
+};
---
+++ labs/lab1/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "calc.h"
+int main()
+{
+
+}
